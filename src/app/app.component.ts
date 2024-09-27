9742cea29e2b5c967faa852445eade900c5d32c0
KMATHIVANAN <kmathivanan@stclaircollege.ca>
2024-09-27 14:00:05 -0400

    In-Class Example Week 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {User} from "./Shared/Models/user";
+import {StudentDetailComponent} from "./student-detail/student-detail.component";
+import {StudentListComponent} from "./student-list/student-list.component";
+import {StudentService} from "./Services/student.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StudentListComponent, StudentDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-Project';
+
 }
---
+++ src/app/student-list/student-list.component.ts
@@ -0,0 +1,33 @@
+import { Component,OnInit } from '@angular/core';
+import {User} from "../Shared/Models/user"
+import {NgFor} from "@angular/common";
+import {StudentDetailComponent} from "../student-detail/student-detail.component";
+import {StudentService} from "../Services/student.service";
+
+
+@Component({
+  selector: 'app-student-list',
+  standalone: true,
+  imports: [NgFor, StudentDetailComponent],
+  templateUrl: './student-list.component.html',
+  styleUrl: './student-list.component.css'
+})
+export class StudentListComponent implements OnInit{
+  userList: User[] = [];
+  displayedColumn:string[] = ['id', 'firstName', 'lastName', 'department', 'isAdmin']
+  constructor(private studentService: StudentService) {
+
+  }
+  ngOnInit() {
+   this.studentService.getStudents().subscribe(
+     {next: (data: User[]) => this.userList=data,
+     error:err => console.error("Error Fetching Students", err),
+     complete:() => console.log("student details fetched")}
+   );
+
+   }
+  selectedStudent?: User;
+  selectStudent(student: User):void{
+    this.selectedStudent = student;
+  }
+}
